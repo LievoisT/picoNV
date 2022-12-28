1ae3eafcbdb0dbf64ea4c0d7900a51e7d1c1cd17
Tanner Lievois <tlievois@gmail.com>
2022-12-28 13:47:15 -0600

    add controls for toggling the blinking led

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import utime
 
 def tick(_tick):
     global led
+    global ledToggle
 
-    led.value(not led.value())
+    if ledToggle:
+        led.value(not led.value())
+    else:
+        led.value(False)
 
 
 led = Pin(25, Pin.OUT)
+ledToggle = True
 # noinspection PyArgumentList
 timer = Timer()
 timer.init(period=1000, mode=Timer.PERIODIC, callback=tick)
@@ -19,11 +24,15 @@ relay = Pin(6, Pin.OUT)  # set pin as output
 while True:
     if select.select([sys.stdin], [], [], 0)[0]:
         ch = sys.stdin.readline()
-        if ch[0] == "1":
+        if ch[0] == '1':
             print('Starting wiper')
             relay(1)
             utime.sleep(3)
             relay(0)
+        elif ch[0] == '2':
+            ledToggle = False
+        elif ch[0] == '3':
+            ledToggle = True
 
     else:
         utime.sleep(1)
