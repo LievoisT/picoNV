960e261a52ed0460b0aa35104f7c28ef71b1fabd
LievoisT <tlievois@gmail.com>
2024-05-09 10:57:09 -0500

    second relay control

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ timer = Timer()
 timer.init(period=1000, mode=Timer.PERIODIC, callback=tick)
 
 relay = Pin(6, Pin.OUT)  # set pin as output
+relay_2 = Pin(7, Pin.OUT)
 while True:
     if select.select([sys.stdin], [], [], 0)[0]:
         ch = sys.stdin.readline()
@@ -29,6 +30,10 @@ while True:
             relay(1)
             utime.sleep(3)
             relay(0)
+        elif ch[0] == '4':
+            relay_2(1)
+        elif ch[0] == '5':
+            relay_2(0)
         elif ch[0] == '2':
             ledToggle = False
         elif ch[0] == '3':
